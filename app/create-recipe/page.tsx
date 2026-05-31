f0410925a4cac577b89a903f832018d056f99b3c
nazar0195 <kcerber777@gmail.com>
2026-05-31 15:54:40 +0200

    Add page: CreateRecipe

--- app/create-recipe/page.tsx
+++ app/create-recipe/page.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import NavBar from '@/components/NavBar'
-import RecipeForm from '@/components/RecipeForm'
-import Footer from '@/components/Footer'
+import NavBar from "@/components/NavBar";
+import RecipeForm from "@/components/RecipeForm";
+import Footer from "@/components/Footer";
+import { FC } from "react";
 
-export default function CreateRecipe(): JSX.Element {
+const CreateRecipe: FC = (): JSX.Element => {
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
-      <main className="flex-grow container mx-auto px-4 py-8">
+      <main className="flex-1 container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-6">Create Recipe</h1>
         <RecipeForm />
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
+
+export default CreateRecipe;
